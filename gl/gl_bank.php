d499887922a1e60ab43fd7046c2081e532ca1c8f
Gittu <Gittu@192.168.137.38>
2015-03-31 01:52:51 +0600

    Merge origin/master
    Conflicts:
    	gl/gl_bank.php

--- gl/gl_bank.php
+++ gl/gl_bank.php
@@ -10,7 +10,7 @@
     See the License here <http://www.gnu.org/licenses/gpl-3.0.html>.
 ***********************************************************************
 *************************COMMIT****************************************
- * sakib
+ * sakib ..... badhan
 ***********************************************************************/
 $path_to_root = "..";
 include_once($path_to_root . "/includes/ui/items_cart.inc");
